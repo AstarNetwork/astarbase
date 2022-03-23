ed477be687edafb8cfb71aaa7b00707e3bd1c12f
Mar.io <34627453+Maar-io@users.noreply.github.com>
2022-03-23 16:21:15 +0100

    Feature/add contract (#6)
    * copy from shidenator branch
    
    * added msg constant
    
    * added comment

---
+++ contract/hardhat.config.js
@@ -0,0 +1,44 @@
+// require('@eth-optimism/smock/build/src/plugins/hardhat-storagelayout')
+require("@nomiclabs/hardhat-ethers");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["storageLayout"],
+        },
+      },
+    },
+  },
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    shibuya: {
+      url: 'https://rpc.shibuya.astar.network:8545',
+      chainId: 81,
+      accounts: [`0x${process.env.ACCOUNT_KEY}`]
+    }
+  }
+};
